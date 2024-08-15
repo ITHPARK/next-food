23a09c4492e364113bf652084bac6cbd1f005518
parktaehyun1005 <ithpark1035@gmail.com>
2024-08-15 17:53:53 +0900

    배포 수정건

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from "../../../../db";
 
 
-export const authOptions: NextAuthOptions = ({
+export const authOptions = {
     session: {
         strategy: "jwt",
         maxAge: 60 * 60 * 24, //초단위로 24시간 유지
@@ -46,7 +46,7 @@ export const authOptions: NextAuthOptions = ({
         return token;
         },
     },
-});
+};
 
 const handler = NextAuth(authOptions);
 
