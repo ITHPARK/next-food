ef7d87e97fe87447234baa9e098df6c0381e1918
parktaehyun1005 <ithpark1035@gmail.com>
2024-08-15 14:09:45 +0900

    오류 수정

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -8,7 +8,7 @@ const Pagination = ({total = 0, page, pathname} : Pagiantion) => {
         <div className='py-6 w-full px-10 flex justify-center flex-wrap gap-4 bg-white my-10 text-black'>
             {total <= 10 ? (
                 [...Array(total)].map((x, i) => (
-                    <Link href={{pathname:pathname, query: {page: i + 1}}}>
+                    <Link href={{pathname:pathname, query: {page: i + 1}}} key={i}>
                         <span className={`px-3 py-2 rounded border shadow-sm bg-white ${
                             i + 1 === parseInt(page, 10)
                             ? "text-blue-600 font-bold"
