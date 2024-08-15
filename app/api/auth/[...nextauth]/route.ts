f3e42fdd564317dee84deacb98fd219a867cd69b
parktaehyun1005 <ithpark1035@gmail.com>
2024-08-15 15:09:19 +0900

    수정

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,5 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import GoogleProvider from "next-auth/providers/google";
-import NaverProvider from "next-auth/providers/naver";
-import KakaoProvider from "next-auth/providers/kakao";
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import NextAuth from 'next-auth';
 import { authOptions } from '../../../lib/auth/authOptions';
-
 
 const handler = NextAuth(authOptions);
 
--- app/lib/auth/authOptions.ts
+++ app/lib/auth/authOptions.ts
@@ -1,48 +1,48 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import GoogleProvider from "next-auth/providers/google";
-import NaverProvider from "next-auth/providers/naver";
-import KakaoProvider from "next-auth/providers/kakao";
+import { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import NaverProvider from 'next-auth/providers/naver';
+import KakaoProvider from 'next-auth/providers/kakao';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from "../../../db";
+import prisma from '../../../db';
 
 export const authOptions: NextAuthOptions = {
     session: {
-        strategy: "jwt",
-        maxAge: 60 * 60 * 24, //초단위로 24시간 유지
-        updateAge: 60 * 60 * 2, //초단위로 2시간 유지
+        strategy: 'jwt',
+        maxAge: 60 * 60 * 24, // 24시간
+        updateAge: 60 * 60 * 2, // 2시간
     },
     providers: [
         GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID || "",
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
         }),
         NaverProvider({
-        clientId: process.env.NAVER_CLIENT_ID || "",
-        clientSecret: process.env.NAVER_CLIENT_SECRET || ""
+            clientId: process.env.NAVER_CLIENT_ID || '',
+            clientSecret: process.env.NAVER_CLIENT_SECRET || ''
         }),
         KakaoProvider({
-        clientId: process.env.KAKAO_CLIENT_ID || "",
-        clientSecret: process.env.KAKAO_CLIENT_SECRET || ""
+            clientId: process.env.KAKAO_CLIENT_ID || '',
+            clientSecret: process.env.KAKAO_CLIENT_SECRET || ''
         })
     ],
-    adapter: PrismaAdapter(prisma), //NextAuth가 prisma와 상호작용을 위한 설정
-    secret: process.env.NEXTAUTH_SECRET, //세션 및 토큰 암호화를 위해 사용된다
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.NEXTAUTH_SECRET,
     pages: {
-        signIn: "/users/login",
+        signIn: '/users/login',
     },
     callbacks: {
-        session: ({ session, token }) => ({
-        ...session,
-        user: {
-            ...session.user,
-            id: token.sub,
-        }
+        session: async ({ session, token }) => ({
+            ...session,
+            user: {
+                ...session.user,
+                id: token.sub,
+            }
         }),
         jwt: async ({ user, token }) => {
-        if (user) {  //현재 세션 데이터를 받아서 객체를 확장시키고 token.sub(사용자의 id)를 session.user.id에 추가한다.
-            token.sub = user.id;
-        }
-        return token;
+            if (user) {
+                token.sub = user.id;
+            }
+            return token;
         },
     },
-    };
+};
