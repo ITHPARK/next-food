9c8a87e1c7a981b8d44a65699cb4d09e6c0e8454
DESKTOP-4ALDHGC\박태현 <ithpark1035@gmail.com>
2024-11-20 20:21:44 +0900

    오류 타입 수정

--- components/DeleteDuplicate.tsx
+++ components/DeleteDuplicate.tsx
@@ -14,7 +14,11 @@ const DeleteDuplicate = () => {
       const data = response.data; // 응답 데이터 받기
       console.log(data.message || '중복된 레코드가 성공적으로 삭제되었습니다!');
     } catch (error) {
-      console.log('중복 삭제에 실패했습니다: ' + error.message);
+      if(error instanceof Error) {// error를 Error 타입으로 캐스팅
+        console.log('중복 삭제에 실패했습니다: ' + error.message);
+      }
+      
+      console.log('오류가 발생했습니다.');
     }
   };
 
